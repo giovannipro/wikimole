c9cc68d6bee55020f05154296d471949d76210e1
giovannipro <lcv-admin@NBMC-541.local>
2014-10-15 00:09:30 +0200

    extract a variable from json

--- export.php
+++ export.php
@@ -1,7 +1,7 @@
 <?php
 
 $pageContent = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllimit=500&titles=Mahatma%20Gandhi');
-$pageContent1 = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=categories&format=json&cllimit=500&titles=Mahatma%20Gandhi');
+//$pageContent1 = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=categories&format=json&cllimit=500&titles=Mahatma%20Gandhi');
 
 //estraggo soltanto il json
 $dom = new DOMDocument();
@@ -19,80 +19,74 @@ $csv_folder  = getenv("DOCUMENT_ROOT").'/wikimole/data/';
 $filename = 'file.txt';
 
 //echo $csv_folder . $CSVFileName;
-
-$myfile = fopen($csv_folder . $filename, "w") or die("Unable to open file!"); //($fileLocation, "w")
-$content = $pageContent . ";<br/>" . $pageContent1;
+/*$myfile = fopen($csv_folder . $filename, "w") or die("Unable to open file!"); //($fileLocation, "w")
+$content = $pageContent; // . $pageContent1;
 fwrite($myfile, $content);
-fclose($myfile);
+fclose($myfile);*/
 
 //stampo il contenuto della query di wikipedia
-echo $content;
+//echo $content;
 
-echo "<br/>";
+//echo "<br/>";
 echo "json creato";
 echo "<br/>";
-echo "ciao 100";
+echo "ciao 082";
+echo "<br/>";
 
 //uso l'estensione json al posto del txt
-$json = rename($csv_folder . $filename, $csv_folder . "file.json");
-$json_parse = json_decode($json,true);//($json, true)
+$jsonTxt = rename($csv_folder . $filename, $csv_folder . "file.json");
+$json = file_get_contents('http://localhost:8888/wikimole/data/file.json');
+$json_parse = json_decode($json,true); //(array)json_decode($json,true);
 
+$jsontest = file_get_contents('http://localhost:8888/wikimole/data/filetest.json');
+$json_parse_test = json_decode($jsontest, true);
 
-$jsontest = file_get_contents('http://localhost:8888/wikipedia/data/filetest.json');
-$json_parse_test = json_decode($jsontest,true);//($json, true)
+//var_dump(json_decode($jsontest, true));
+/*var_dump($json_parse_test);
+echo '<br/>';*/
 
-/*
-foreach($json_parse['query']['pages'] as $item) {
-    print $item['query'];
-    print ' - ';
-    print $item['pageid'][0]['value'];
-}
+//var_dump($json_parse[query][pages][19379][langlinks]);
+//echo '<br/>';
 
-$output_test = "<ul>";
-foreach ($json_parse_test["employees"] as $employees) {
-	$output_test .= "<li>" . $employees["firstName"] . "</li>";
-	//$output .= "<li>" . $19379["title"] . "</li>";
-}
-$output_test .= "</ul>";
+$lang = [];
 
-echo $output_test;
-echo "<br/>";
+foreach($json_parse[query][pages][19379][langlinks] as $p) {
+    $lang[] = 'Lang: '. $p[lang];
+};
 
-
-$output = "<ul>";
-foreach ($json_parse->query as $query {
-	$output .= "<li>" . "{$query->name}\n" . "</li>";
-}
-    foreach ( $json_parse->query as $query )
-    {
-    	echo "{$query->pages}\n";
-    }
-$output .= "</ul>";
-
-
-
-echo $output;
-echo "<br/>";
+$myArray = json_encode($lang);
+echo $myArray;
+echo '<br/>';
 
 /*
-$pageid = $json_parse['query']['pages'][0]['pageid'];
-
-echo $pageid = $json_parse->{'query'}->{'pages'}->{'19379'}[0]->pageid;
-
-echo $pageid;
-echo "<br/>";
-
-//listing the array
-foreach($json_parse as $prop => $value)
-echo '<br/>'. $prop.' : '. $value;
-
-echo "<br/>";
-print_r($json_parse);
-echo "<br/>";
-var_dump($json_parse);
+foreach($json_parse_test[person] as $p) {
+    echo 'Name: '.$p[name][first].' '.$p[name][last].'Age: '.$p[age].'';
+}
+echo '<br/>';
 */
 
-/* Riferimenti
+$array = array(
+    'name' => 'ben',
+    'age' => 23,
+    'skills' => array(
+        'php', 'css', 'javascript'
+    )
+);
+
+$jsonb = json_encode($array);
+
+echo $jsonb;
+echo '<br/>';
+
+
+/* 
+
+JSON validator
+http://jsonlint.com/
+
+References
+http://codular.com/json
+http://www.academia.edu/4092169/Get_data_from_string_with_JSON_object_in_PHP
 http://www.developersnacks.com/2012/06/export-html-to-csv-in-php.html
 http://stackoverflow.com/questions/8858848/php-read-and-write-json-from-file
 
