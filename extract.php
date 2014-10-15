836e73090f1a6739838d95905168a8fbbb3cd063
Giovanni <g.profet@gmail.com>
2014-10-15 16:15:50 +0200

    Add Angular and templates api

--- extract.php
+++ extract.php
@@ -3,7 +3,7 @@
 echo "extract.php";
 echo "<br/>";
 
-echo "ciao 030";
+echo "ciao 044";
 echo "<br/>";
 
 //richiamo il json con il nome degli articoli
@@ -34,8 +34,69 @@ $json_parse = json_decode($json,true);
 */
 
 //cerco api per lingua e categoria per un solo articolo
-$apiLang = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllimit=500&titles=Mahatma%20Gandhi');
-$apiCat = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=categories&format=json&cllimit=500&titles=Mahatma%20Gandhi');
+$apilang = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllimit=500&titles=Mahatma%20Gandhi');
+$apicat = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=categories&format=json&cllimit=500&titles=Mahatma%20Gandhi');
+
+$template = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=transcludedin&titles=Mahatma%20Gandhi');
+echo $template;
+echo "<br/>";
+
+$html = file_get_contents('https://en.wikipedia.org/w/index.php?title=Mahatma_Gandhi&action=info#mw-pageinfo-watchers');
+//$ret = $html->find('body');
+//$html_a->load("https://en.wikipedia.org/w/index.php?title=Mahatma_Gandhi&action=info#mw-pageinfo-watchers");
+
+echo "gli echo qui sotto non funzionano";
+//echo $html;
+//echo "<br/>";
+
+//echo $ret;
+//echo "<br/>";
+
+echo $html->getElementById('content');
+echo "<br/>";
+
+
+/*
+$templateb = file_get_contents('https://en.wikipedia.org/w/index.php?title=Mahatma_Gandhi&action=info#mw-pageinfo-watchers');
+echo $templateb;
+echo "<br/>";
+*/
+
+/*estraggo soltanto la lista dei template
+$dom_a = new DOMDocument();
+$dom_a->loadHTML($templateb);
+
+$xpath_1 = new DOMXPath($dom_a);
+$list_a = $xpath_1->query('//tr[@id="mw-pageinfo-templates"]'); //('//div[@class="text"]')
+
+foreach ($list_a as $list_b) {
+    var_dump($list_b->lsitContent);
+}*/
+
+/*
+$html = file_get_html('https://en.wikipedia.org/w/index.php?title=Mahatma_Gandhi&action=info#mw-pageinfo-watchers');
+$x = $html->find('div.mw-body',0); //.content
+echo $x;
+echo "<br/>";
+
+include "simple_html_dom.php";
+$html = new simple_html_dom();
+$html->load_file('https://en.wikipedia.org/w/index.php?title=Mahatma_Gandhi&action=info#mw-pageinfo-watchers');
+$con_div = $html->find('div',0);//get value plaintext each html
+
+
+// Create DOM from URL or file
+$html = file_get_html('https://en.wikipedia.org/w/index.php?title=Mahatma_Gandhi&action=info#mw-pageinfo-watchers');
+
+// Find all images
+foreach($html->find('ul') as $element_a){
+    echo $element_a->li . '<br>';
+}*/
+
+/*
+echo $con_div;
+echo "<br/>";
+*/
 
 /*
 foreach($articles[articles] as $c) {
@@ -58,10 +119,16 @@ foreach ($tags as $tag) {
 
 /* 
 
+------------------------------------------
+References
+------------------------------------------
+
+Estrarre contenuti da pagina html
+http://simplehtmldom.sourceforge.net/manual.htm
+
 JSON validator
 http://jsonlint.com/
 
-References
 http://codular.com/json
 http://www.academia.edu/4092169/Get_data_from_string_with_JSON_object_in_PHP
 http://www.developersnacks.com/2012/06/export-html-to-csv-in-php.html
--- parsing.php
+++ parsing.php
@@ -14,25 +14,23 @@ $filenameb = 'data.txt';
 
 //uso l'estensione json al posto del txt
 $jsonTxt = rename($csv_folder . $filename, $csv_folder . "file.json");
-$json = $apiLang; //file_get_contents('http://localhost:8888/wikimole/data/file.json');
-$json_parse = json_decode($json,true); //(array)json_decode($json,true);
+$jsonlang = $apilang;
+$jsoncat = $apicat;
 
-//$jsontest = file_get_contents('http://localhost:8888/wikimole/data/filetest.json');
-//$json_parse_test = json_decode($jsontest, true);
-
-//$jsontestb = $apiCat;//file_get_contents('http://localhost:8888/wikimole/data/filetestb.json');
-//$jsonb_parse_test = json_decode($jsontestb, true);
+//decodifico i json
+$jsonlang_parse = json_decode($jsonlang,true);
+$jsoncat_parse = json_decode($jsoncat,true);
 
 //definizione dei valori sotto forma di array
 $lang = [];
 $cat = [];
 
 //estraggo una serie di valori dal json
-foreach($json_parse[query][pages][19379][langlinks] as $a) {
+foreach($jsonlang_parse[query][pages][19379][langlinks] as $a) {
     $lang[] = $a[lang];//"Lang: ". $a[lang];
 };
 
-foreach($jsonb_parse_test[query][pages][19379][categories] as $b) {
+foreach($jsoncat_parse[query][pages][19379][categories] as $b) {
     $cat[] = $b[title]; // 'Cat: '. $b[title];
 };
 
