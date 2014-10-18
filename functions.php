849dfbf2c1e3fe33b7f4023e4f4f054d7203b5e7
giovannipro <lcv-admin@NBMC-541.local>
2014-10-19 01:14:10 +0200

    two articles in one json

--- functions.php
+++ functions.php
@@ -5,8 +5,8 @@ function add($a,$b) {
 }
 
 // scrivo il json con lingua e categoria
-function lang_cat($a,$b,$c) {
-	return '{'.$a.': {"Language": '.json_encode($b).',"Category": '.json_encode($c).'}}';
+function lang_cat($a,$a1,$a2,$b,$b1,$b2) {
+	return '{"Articles": [{"'.$a.'" : {"Language": '.json_encode($a1).',"Category": '.json_encode($a2).'}},{"'.$b.'" : {"Language": '.json_encode($b1).',"Category": '.json_encode($b2).'}}]}';
 }
 
 
--- parsing.php
+++ parsing.php
@@ -6,7 +6,7 @@ require_once('functions.php');
 error_reporting(~0);
 ini_set('display_errors', 1);
 
-echo "ciao 008";
+echo "ciao 040";
 echo "<br/>";
 
 echo "parsing.php";
@@ -16,20 +16,39 @@ echo "<br/>";
 error_reporting(~0);
 ini_set('display_errors', 1);
 
+
 //indico il nome del file e la sua posizione
 $csv_folder  = getenv("DOCUMENT_ROOT").'/wikimole/data/';
-$filename = 'file.txt';
-$filenameb = 'data.txt';
+//$filename = 'file.txt';
+$filenameb = 'data.json';//'data.txt';
 
 //uso l'estensione json al posto del txt
-$jsonTxt = rename($csv_folder . $filename, $csv_folder . "file.json");
+//$jsonTxt = rename($csv_folder . $filename, $csv_folder . "file.json");
+
+//cerco api per lingua e categoria per due articoli
+$apilang = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllimit=500&titles=Mahatma%20Gandhi');
+$apicat = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=categories&format=json&cllimit=500&titles=Mahatma%20Gandhi');
+
+$apilang_b = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllimit=500&titles=Nelson%20Mandela');
+$apicat_b = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&prop=categories&format=json&cllimit=500&titles=Nelson%20Mandela');
+
+
+
 $jsonlang = $apilang;
 $jsoncat = $apicat;
 
-//decodifico i json
+$jsonlang_b = $apilang_b;
+$jsoncat_b = $apicat_b;
 
+
+
+//decodifico i json
 $jsonlang_parse = json_decode($apilang,true);
 $jsoncat_parse = json_decode($apicat,true);
+
+$jsonlang_parse_b = json_decode($apilang_b,true);
+$jsoncat_parse_b = json_decode($apicat_b,true);
+
 //$jsonlang_all_parse = json_decode($apilink_all,true);
 //$jsontemp_parse = json_decode($apitemp,true);
 
@@ -37,36 +56,48 @@ $jsoncat_parse = json_decode($apicat,true);
 $lang = [];
 $cat = [];
 
+$lang_b = [];
+$cat_b = [];
+
 //estraggo una serie di valori dal json
-foreach($jsonlang_parse[query][pages][19379][langlinks] as $a) {
-    $lang[] = $a[lang];//"Lang: ". $a[lang];
+foreach($jsonlang_parse['query']['pages']['19379']['langlinks'] as $a) {
+    $lang[] = $a['lang'];//"Lang: ". $a[lang];
 };
 
-foreach($jsoncat_parse[query][pages][19379][categories] as $b) {
-    $cat[] = $b[title]; // 'Cat: '. $b[title];
+foreach($jsoncat_parse['query']['pages']['19379']['categories'] as $a) {
+    $cat[] = $a['title']; // 'Cat: '. $b[title];
 };
 
-/*foreach($jsonlang_all_parse[query][pages][0][langlinks] as $c) {
-    $lang_all[] = $b[title]; // 'Cat: '. $b[title];
-};*/
+foreach($jsonlang_parse_b['query']['pages']['21492751']['langlinks']  as $a){  //--->non funziona
+    $lang_b[] = $a['lang'];//"Lang: ". $a[lang];
+};
+
+foreach($jsoncat_parse_b['query']['pages']['21492751']['categories']  as $a){  //--->non funziona
+    $cat_b[] = $a['title'];//"Lang: ". $a[lang];
+};
+
+
+
+//$secondo_art = '{"21492751": {"Language": '.json_encode($lang_b).',"Category": '.json_encode($cat_b).'}}';
+//echo $secondo_art."<br/>";
+
+
+
 
 //scrivo il contenuto del file json con i due insiemi di dati
 //$twodata = '{"19379": {"Language": '.json_encode($lang).',"Category": '.json_encode($cat).'}}';
-$twodata = lang_cat('"19379"',$lang,$cat);
-
-//$myfunction = add(50,30);
-//echo $myfunction;
+$twodata = lang_cat("art_1",$lang,$cat,"art_2",$lang_b,$cat_b);
+echo $twodata."<br/>";
 
 
-//echo $articles_all;
-echo $twodata;
+
 
 //scrivo il file json con i due insiemi di dati
 $jsonMerged = fopen($csv_folder . $filenameb, "w") or die("Unable to open file!"); 
 $content = $twodata;
 fwrite($jsonMerged, $twodata);
 fclose($jsonMerged);
-$jsonFile = rename($csv_folder . $filenameb, $csv_folder . "data.json");
+//$jsonFile = rename($csv_folder . $filenameb, $csv_folder . "data.json");
 
 
 /* 
