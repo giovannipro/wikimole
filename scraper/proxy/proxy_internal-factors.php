bb30abc643028d39ff432c720c9d4dd8fbd4d27f
giovannipro <lcv-admin@NBMC-541.local>
2015-08-04 00:46:59 +0200

    some tests
    about interlinks (remove external links), and internal factors

--- scraper/proxy/proxy_internal-factors.php
+++ scraper/proxy/proxy_internal-factors.php
@@ -2,13 +2,12 @@
 	//echo file_get_contents($_POST['http://it.wikipedia.org/wiki/JavaScript_Object_Notation']);
 	
 	header('Access-Control-Allow-Origin: *');
-	header('Content-Type: application/json');
 
 	if (!isset($_GET['url'])) {
 	    die(); // Don't do anything if we don't have a URL to work with
 	}
 	$url = urldecode($_GET['url']);
-	$url = 'http://' . str_replace('http://', '', $url); // Avoid accessing the file system
+	$url = 'https://' . str_replace('https://', '', $url); // Avoid accessing the file system
 
 	//$id = $doc->getElementById('#content')
 
