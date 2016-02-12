75a8a4c0b707b7d0d464631ef970d29732cd6f0b
giovannipro <lcv-admin@NBMC-541.local>
2016-02-12 17:27:36 +0100

    test

--- scraper/lib/functions/revision_editor.js
+++ scraper/lib/functions/revision_editor.js
@@ -8,17 +8,29 @@ SOURCE
 
 var baseurl = 'http://localhost:8888/wikimole/scraper/proxy/';
 var wikilink = 'https://en.wikipedia.org/wiki/';
-var edits_15 = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=timestamp|user|size&rvlimit=1000&rvstart=1420066800&rvend=1448924400&rvdir=newer&indexpageids=&titles='; 
-var edits_14 = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=timestamp|user|size&rvlimit=1000&rvstart=1388530800&rvend=1419980400&rvdir=newer&indexpageids=&titles=';
+var edits_14 = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=timestamp|user|size&rvlimit=1000&rvstart=1420070399&rvend=1388534400&rvdir=newer&indexpageids=&titles='; 
+var edits_15 = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&rvprop=timestamp|user|size&rvlimit=1000&rvstart=1451606399&rvend=1420070400&rvdir=newer&indexpageids=&titles=';
 var backlinks = 'https://en.wikipedia.org/w/api.php?action=query&list=backlinks&bllimit=1000&format=json&bltitle='
-var user_contributs = 'https://it.wikipedia.org/w/api.php?action=query&list=usercontribs&format=json&ucnamespace=0&ucprop=title|timestamp|size|sizediff|flags&ucuser'
+var user_contributs_14 = 'https://it.wikipedia.org/w/api.php?action=query&list=usercontribs&format=json&ucstart=1420070399&ucend=1388534400&ucprop=ids|title|timestamp|comment|parsedcomment|size|sizediff|flags|tags&ucuser='
+var user_contributs_15 = 'https://it.wikipedia.org/w/api.php?action=query&list=usercontribs&format=json&ucstart=1451606399&ucend=1420070400&ucprop=ids|title|timestamp|comment|parsedcomment|size|sizediff|flags|tags&ucuser='
+
+
+/*
+http://www.epochconverter.com/
+
+1.01.2014, 00:00:00     1388534400 
+31.12.2014, 23:59:59    1420070399
+
+1.01.2015, 00:00:00     1420070400
+31.12.2015, 23:59:59    1451606399
+*/
 
 
 /* ------------------------------------
 ARTICLES LIST
 -------------------------------------*/
 
-var art_list = '../articles/updated_articles_list.json'; //  articles_test    / updated_articles_list
+var art_list = '../articles/articles_test.json'; //  articles_test    / updated_articles_list
 
 var list = [
 	'nelson_mandela',
@@ -26,6 +38,15 @@ var list = [
     'AIDS',
     'sexism'
 ]	
+
+var editors = [
+    'iopensa',
+    'RebeccaCasper',  //
+    'Mean_as_custard',
+    'Nonsurgicalliposection',
+    'Nanno1992',
+    'example'
+]
 
 // get the list of articles
 $.getJSON(art_list, function(mydata) {
@@ -192,28 +213,22 @@ function get_editors(url) {
             
             obj = []
             obj = $(wikiResponse.query.pageids)
-
             pageids = obj[0].toString()
             edit =  $(wikiResponse.query.pages)[0][pageids].revisions
 
-            sum_edits = 0
-            sum_editors = 0
-            sum_size = 0
-
             findme1 = 'bot'
 
-            size_array = []
             user_array = []
-
-            var result = [];
+            result = [];
 
             var sumX = 0
             var total = 0
 
+            console.log(edit)
+
             jQuery.each( edit, function( a,b ) {
 
                 user = b.user
-                size = b.size
 
                 if ( user.toLowerCase().indexOf(findme1) >= 0 ) {
                         
@@ -221,8 +236,6 @@ function get_editors(url) {
 
                 }
                 else {
-                    //sum_edits++
-                    
                     user_clean = user.replace(/./g,'_').replace(/ /g,'_')
 
                     user_array.push( user );
@@ -242,14 +255,9 @@ function get_editors(url) {
 
             })
 
-            //test1 = result.replace(/./g,'_').replace(/ /g,'_')
-
-            console.log(result)
-
             test = JSON.stringify(result);
-
-            //container.append(url_clean + ':<br>')
-            container.append(test + '<br>')
+            container.append(result + ',<br>')
+            console.log(result)
 
             $('.hide_1').hide()
         },   
@@ -267,6 +275,70 @@ function get_all_editors() {
     //container.append('article,edits,unique_editors,avg_size<br/>')
     jQuery.each( articles, function( i, val ) {
         get_editors( edits_15 + val )
+        console.log(val)
+    })  
+}
+
+/* ------------------------------------
+EDITED ARTICLES
+-------------------------------------*/
+
+function get_edited_articles(url) {
+    
+    $.ajax(url, {
+        dataType:  "jsonp",
+        success: function( wikiResponse ) {
+
+            container = $('#edits');
+            url_clean = url.replace(user_contributs_15,'').replace('_',' ')
+
+            user_art = $(wikiResponse.query.usercontribs)
+            
+            //console.log(wikiResponse)
+            console.log(user_art)
+    
+            result = []
+            art_array = []
+
+            jQuery.each( user_art, function( a,b ) {
+
+
+                art = b.title
+                art_array.push(art)
+
+                art_array_sort =  art_array.sort()
+                
+                $.each(art_array_sort, function(i, e) {
+                    if ($.inArray(e, result) == -1) {
+                        result.push(e);
+                    }
+
+                })
+                
+                sorted_result = result.sort()
+
+            })
+
+            console.log(result)
+            container.append(url_clean + '<br>')
+            container.append(sorted_result + ',<br>')
+
+            $('.hide_1').hide()
+        },   
+        error : function (xhr, ajaxOptions, thrownError) {
+            console.log(xhr.status);
+            console.log(thrownError);
+        }
+    })
+
+}
+
+// get entry links for all of article
+function get_all_edited_articles() {
+    var container = $('#edits')
+    //container.append('article,edits,unique_editors,avg_size<br/>')
+    jQuery.each( editors, function( i, val ) {
+        get_edited_articles( user_contributs_15 + val )
         console.log(val)
     })  
 }
