720ab4d45e227d98e82d537059bd7b3de1fe01f2
Giovanni <g.profet@gmail.com>
2017-08-07 18:51:47 +0200

    make the timeline of page views

---
+++ scraper/python/scrape_page_views.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# Scrape page views
+
+import os					# get file path
+import webbrowser			# open webpages
+import time					# get unix code
+import datetime				# convert in unix timestamp
+import urllib, json, io		# read json
+from urllib import urlopen	# open file
+import sys					# reset file encoding
+import csv					# read csv
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+# -----------------------------------
+# Utilities
+
+folder = "/Applications/MAMP/htdocs/wikimole/scraper/" # os.path.dirname(os.path.realpath(__file__))
+t = "\t"
+n = "\n"
+lic = ""
+s = " "
+
+# -----------------------------------
+# Main variables
+
+api_old = "http://stats.grok.se/json/en/" # 2008 — 2015
+api = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/" # from 2016 
+
+root_api = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/"
+
+# https://tools.wmflabs.org/pageviews/?project=en.wikipedia.org&platform=all-access&agent=user&range=latest-20&pages=Cat|Dog
+# http://stats.grok.se/json/en/200801/hiv
+# https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/Day_of_Reconciliation/daily/2015070100/2017060100
+# https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/Day_of_Reconciliation/monthly/20120101/20161231
+
+# -----------------------------------
+# Script
+
+def time():
+	my_format = "%d %m %Y %I:%M%p" 
+	ts = datetime.datetime.utcnow().strftime(my_format)
+	print(ts)
+
+def add_zero(x):
+	if x < 10:
+		return "0" + str(x)
+	else:
+		return x
+
+def scrape_pv_after_2015(art_list,start,end): # from July 2015
+	# print(article,start,end)
+	time()
+
+	f_in = folder + "raw_data/" + art_list + ".json"  # test / data
+	f_out = folder + "raw_data/" + art_list + "-pv.tsv" 
+	
+	with open(f_in, "r") as f1:
+		with open(f_out, "w") as f2:
+
+			articles = json.load(f1)
+			index = 0
+
+			# lang, article,start,end
+
+			for row in articles:
+				try:
+					title = row["title"].replace("!","%21")					
+					# print (title)
+
+					index += 1
+
+					timespan = start + "/" + end
+					# request = root_api + lang + ".wikipedia/all-access/user/" + article + "/daily/" + timespan
+					request = api + title + "/daily/" + timespan
+					# print request
+
+					response = urlopen(request).read()
+					data = json.loads(response)
+					
+					for a in data.items(): # .items()
+						try:
+							values = a[1]
+
+							for x in values:
+								views = x["views"]
+								timestamp = x["timestamp"]
+								output = str(index) + t + title + t + timestamp + t + str(views)
+								output_ = output + n
+								print output
+
+								f2.write(output_)
+						except:
+							print("error_2", request)
+							pass
+				except:
+						output = str(index) + t + request
+						output_ = output + n
+						print("error_1", output)
+						# f2.write(output_)
+						pass
+
+	time()
+
+def scrape_pv_before_2015(article,start,end): 
+	print(article,start,end)
+
+	for t in range (start,(end + 1)):
+
+		for x in range (1, 13):
+			timespan = str(start) + str(add_zero(x))
+
+			request = api_old + timespan + "/" + article
+			print request
+			
+			# response = urlopen(request).read()
+			# data = json.loads(response)
+
+			# for key, value in data["daily_views"].items():
+			# 	date = str(key)
+			# 	views = str(value)
+			# 	output = date + "\t" + views
+
+			# 	print(output)
+
+		start += 1
+
+
+# -----------------------------------
+# Launch script
+
+#scrape_pv_before_2015("Day_of_Reconciliation",2004,2015)
+scrape_pv_after_2015("articles_test","20150701","20170630") #  articles_details articles_test 20150701 20170630 // 20170601 20170602
+
+#scrape_pv_after_2016("en","Wikipedia:Graphics Lab/FPhotography workshop","20161231","20170330")
+#scrape_pv_before_2015("Day_of_Reconciliation",2008,2015)
+
+
+
+
+############
+
+# https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/Wikipedia%3AGraphics_Lab%2FPhotography_workshop/daily/2017042400/2017051400
+# https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/Wikipedia%3Graphics_Lab%2FPhotography_workshop/daily/2016070100/2016033100
+
+
+
+
+
+
+
+
+
---
+++ views/dataviz/pageviews/pageviews.js
@@ -0,0 +1,269 @@
+// main variables
+// ------------------
+
+var w = window;
+
+var width = w.outerWidth;
+var height = width * 7.4; //3000; //width + (width*0.3);
+
+var margin = {top: 50, right: 50, bottom: 50, left: 50},
+    nomargin_w = width - (margin.left + margin.right),
+    nomargin_h = height - (margin.top + margin.bottom);
+
+var padding = width/100,
+    offset = padding * 1.5,
+    bar_h = 6;
+
+// var streamHeight = 20;
+    // dateFormat_edit = d3.time.format('%Y-%m-%d'), // 01.01.14 // %d-%m-%y"  // %d.%m.%y
+
+var dateFormat_pageview = d3.time.format('%Y%m%d%H'); // 2015070100
+
+var v_shift = 100,
+    label_space = 300;
+
+var fontsize = 16,
+    fontweight = 300;
+
+// var n_art = 177; // 177 9
+
+// set plot
+// ------------------
+
+var svg = d3.select("#svg_container")
+    .append("svg")
+    .attr("viewBox", '0 0 ' + width + ' ' + (height) )
+
+var plot = svg.append("g")
+    .attr("id", "d3_plot")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// get data
+// ------------------
+
+d3.csv("../../assets/data/20170803/pageview_2015_2017.csv", horizon_chart); // pageview_2015_2017 pageview_2015_2017_test  horizon_chart loaded_pageview
+// d3.csv("../../assets/data/20160227/edit.csv", loaded_pageview); 
+
+function horizon_chart (error, data){
+    if (error) throw error;
+
+// data
+    var dateFormat_pageview = d3.time.format('%Y%m%d%H'); // 2015070100
+
+    data.forEach(function(d) {
+        d.id = +d.id;
+        d.article = d.article;
+        d.timestamp = dateFormat_pageview.parse(d.timestamp);
+        d.pageviews = +d.pageviews;
+    })  
+
+    var timeDomain = d3.extent(data, function(d){return d.timestamp});
+
+    var nest = d3.nest()
+        .key(function(d) { return d.article; })
+        .rollup(function (g) {
+            return g.map(function (d) {
+                return [d.timestamp, d.pageviews]
+            })
+            // .sort( function(a,b){return a[0] - b[0]}) //sort temporally
+        })
+        .entries(data)
+    // console.log(data[0].pageviews)
+
+    nest.sort(function(a,b){
+        return a.values.reduce(function (c,d) {return c + d[1]}, 0) - b.values.reduce(function (c,d) {return c + d[1]}, 0)
+    })
+    console.log(nest)
+
+// axis
+
+    var x = d3.time.scale()
+        .domain(timeDomain)
+        .range([0,nomargin_w - (label_space + 10)]); //margin.left - margin.right 
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient('top')
+        .ticks(d3.time.months, 1)
+        .tickFormat(d3.time.format('%y.%m'))
+        .tickSize(5)
+        .tickSubdivide(true)
+
+    var o_line = plot.append('g')
+        .attr("transform","translate(" + 0 + ",0)")
+        .attr('class','v_lines')
+        .call(xAxis)
+        .selectAll('text')
+            .attr("transform", "rotate(0)")
+            .style("text-anchor", "start")
+            .attr('fill',"red")
+            .attr("font-size",fontsize)
+
+// visual elements
+
+    var horizon = d3.horizon()
+        // .scale(scale() ? "global" : "local")
+        .width(nomargin_w - (label_space + 10))
+        .height(v_shift)
+        .bands(5) // 1 - 5
+        .mode("mirror") // mirror offset
+        .interpolate("basis")
+
+    var article = plot.selectAll('.article')
+        .data(nest)
+        .enter()
+        .append('g')
+        .attr("class","article")
+        .attr("id",function (d,i){
+            return (i+1) + "-" + d.key
+        })
+        .attr("transform",function (d,i){
+            return "translate(0" + "," + (v_shift * i) + ")" 
+        })
+        // .call(horizon);
+
+    var text = article.append("g")        
+        .attr("transform", function(d,i){
+            return "translate(" + (nomargin_w - label_space) + "," + -10 + ")"
+        })
+        .append("text")
+        .attr("x", 0)
+        // .attr("dy",5)
+        .attr("y", function(d){
+            return (fontsize * 2)
+        })        
+        .style("font-size",fontsize)
+        .style("font-weight",fontweight)
+        .style("letter-spacing",0)
+        .style("text-anchor","start") // end
+        .text(function(d,i){
+            return (d.key)
+        })
+
+    var horizon = article.selectAll('horizon')
+        .data(function (d) {
+            return [d.values];
+        })
+        .enter().append('g')
+        .call(horizon);
+};
+
+function loaded_pageview (data){
+
+    data.forEach(function(d) {
+        d.id = +d.id;
+        d.article = d.article;
+        d.timestamp = dateFormat_pageview.parse(d.timestamp);
+        d.pageviews = +d.pageviews;
+    })
+    // console.log(data[0])
+
+    // set x and y domain
+    var pvDomain = d3.extent(data, function(d){return d.pageviews});
+    var timeDomain = d3.extent(data, function(d){return d.timestamp});
+
+    var max_pageviews = d3.max(data, function(d) {return d.pageviews}),
+        max_pv = 1000;
+
+    // nest dataset for every article 
+    data = d3.nest().key(function(d){return d.article}).entries(data);
+
+    // sort in descending order of total amount of edits
+    data.sort(function(a,b){
+        var a = d3.sum(a.values, function(d){return Math.abs(d.pageviews)  })
+        var b = d3.sum(b.values, function(d){return Math.abs(d.pageviews)  })
+        return a - b
+    })
+    
+    console.log(data)
+    console.log(pvDomain, max_pageviews)
+
+// set axis
+// ------------------
+
+    // var height_stream = data.length * streamHeight
+
+    // set x range
+    var x = d3.time.scale()
+        .domain(timeDomain)
+        .range([0, nomargin_w]); //margin.left - margin.right 
+
+    // set x axis
+    // var xAxis = d3.svg.axis()
+    //     .scale(x)
+    //     .orient('bottom')
+    //     .ticks(d3.time.months, 1)
+    //     .tickFormat(d3.time.format('%m'))
+    //     .tickSize(10)
+    //     .tickSubdivide(true)
+
+    // set y range
+    var y = d3.scale.ordinal()
+        .domain([0,max_pv]) // max_pageviews
+        // .data.map(function(d){return d.key})
+        // .rangePoints([0, 100], 1 ); // height_stream
+        .range([0, 50]); // v_shift
+
+    // set y axis  
+    // var yAxis = d3.scale.linear()
+    //     .domain(sizeDomain)
+    //     .range([0, v_shift]); //height_stream - margin.top - margin.bottom ]);
+
+    // area generator
+    var area = d3.svg.area()
+        .interpolate("linear") // bundle
+        .x(function(d) { 
+            return x(d.timestamp) //dateFormat_pageview.parse(d.timestamp)) //x(d.timestamp)
+        }) 
+        .y0(function(d) {
+            return 0 // -(( ( (nomargin_h/n_art) * 20 ) * (d.pageviews )) / max_pageviews ) 
+        })     
+        .y1(function(d) {
+            return -( (v_shift * d.pageviews) / max_pv) // max_pageviews    // -(( ( (nomargin_h/n_art) * 20 ) * (d.pageviews )) / max_pageviews ) 
+        })
+        
+// visualize elements
+// ------------------
+
+    // articles
+    var articles = plot.selectAll("g")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr('class','article')
+        .attr('id',function(d,i){
+            return (i+1) + '-' + d.key
+        })
+        .attr("transform", function(d, i) {
+            return 'translate(' + 0 + ',' + (v_shift * (i+1))  + ')' // shift ((nomargin_h/n_art)*i )
+        }) 
+
+    // pageviews
+    var pv = articles.append('g')
+        .attr('class','pageviews')
+        .append("path")
+        //.attr("transform", function(d, i) { return "translate(" + shift + "," + (y(d.key) + padding )   + ")" }) // 
+        .style("fill", "red")
+        .style("stroke", "white") 
+        .style("stroke-width", "1")
+        .attr("d", function(d){ 
+            return area(d.values) 
+        })
+
+    // var text = articles.append("text")
+    //     .attr("x", -10)
+    //     .attr("dy",5)
+    //     .attr("y", function(d){
+    //         return y(d.key) + padding 
+    //     })
+    //     .attr("transform", function(d,i){
+    //         return "translate(" + (nomargin_w - 200) + "," + -10 + ")"
+    //     })
+    //     .style("font-size",30) // fontsize
+    //     .style("font-weight",fontweight)
+    //     .style("letter-spacing",1)
+    //     .style("text-anchor","end")
+    //     .text(function(d,i){
+    //         return (d.key)
+    //     })
+}
